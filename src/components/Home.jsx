286622e637ab650ac98137a4a6e7548c1c8adbfd
samdish jadhav <samdishjadhav9970@gmail.com>
2023-10-29 22:55:58 +0530

    home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,22 @@ import HomeHeader from "./HomeHeader";
 import SearchInput from "./SearchInput";
 import Footer from "./Footer";
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="flex h-[100vh] flex-col ">
+            <HomeHeader />
+            <main className="grow flex justify-center">
+                <div className="w-full px-5 flex flex-col items-center mt-44">
+                    <img src={Logo} className="w-[172px] md:w-[272px] mb-8 " alt="" />
+                    <SearchInput />
+                    <div className="flex gap-2 text-[#3c4043] mt-8">
+                        <button className="h-9 px-4 text-sm rounded-md border border-[#f8f9fa] bg-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2 ">Google Search</button>
+                        <button className="h-9 px-4 text-sm rounded-md border border-[#f8f9fa] bg-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2 ">I'm Feeling Lucky</button>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </div>
+    )
 };
 
 export default Home;
--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -4,7 +4,16 @@ import Profile from "../assets/profile-200x200.jpg";
 import ProfileRing from "../assets/profile-ring.svg";
 
 const ProfileIcon = () => {
-    return <div>Profile Icon</div>;
+    return (
+        <div className="flex gap-2">
+            <span className="h-10 w-10 flex items-center justify-center rounded-full cursor-pointer hover:bg-black/[0.05] ">
+                <TbGridDots size={20} color="#5f6368" />
+            </span>
+            <span className="h-10 w-10 flex items-center justify-center relative cursor-pointer hover:bg-black/[0.05] ">
+                <img className="" src={ProfileRing} alt="" />
+            </span>
+        </div>
+    )
 };
 
 export default ProfileIcon;
